eda228dbe634d5902fe3f90608b85f5440718ac4
Felix Jansson <felixjansson.96@gmail.com>
2018-10-04 09:49:53 +0200

    sorting

--- Wpt.m
+++ Wpt.m
@@ -1,7 +1,8 @@
 
 % TODO, make a function
-
 clear all; clc;
+dwtmode('per');
+
 
 % Test data 
 load('galax2.mat');
@@ -15,21 +16,32 @@ image_padded(1:size(image,1),1:size(image,2)) = image;
 t = wpdec2(image_padded,3,wname);
 plot(t)
 
+%128^2 
+tn_length = size(get(t,'tn'),1) - 1;
+allcfs = [];
+for i = 1 :+1:tn_length
+    cfs = read(t,'cfs',[3,i]);
+    allcfs = vertcat(allcfs,cfs(:));
+end
+
+[allcfsSorted, sortind] = sort(allcfs, 2, 'descend');
+
 % Visit all terminal nodes.
 tn_length = size(get(t,'tn'),1) - 1;
-for i = 0 :+1:tn_length
+for i = 1 :+1:tn_length
     % Get all coefficients of the terminal node 
     cfs = read(t,'cfs',[3,i]);
     
     % Get all coefficients seperately of the terminal node 
-    [t1,cA,cD] = wpsplt(t,[3 i]);
+    % [t1,cA,cH,cV,cD] = wpsplt(t,[2 i]);
     
     % Test Set all values in cfs to zero.
     newCFS = zeros(size(cfs));
     t = write(t,'cfs',[3 i],newCFS);
 end
 
+% Should be a total black image
+rec_image = wprec2(t);
+figure; imshow(rec_image,[])
 
-   
-    
     
