85a540ff1ee6efdb8b28ab49665573bc5efec8ea
Adrian Leidegren <leidegrk@net.chalmers.se>
2018-10-08 15:11:38 +0200

    Chose images and removed the rest. Hurrah!

--- mainscript.m
+++ mainscript.m
@@ -4,28 +4,13 @@ dwtmode('per');
 % Load images
 load galax2.mat %ngc3314
 clear map_ngc3314 % map_ngc3314 is just gray colormap.
-duststorm = double(imread('Dust_storm_in_Amarillo,_Texas.gif'));
-footprint = double(imread('610px-Footprint.gif'));
-footprint512 = double(imread('Footprint512.tiff'));
-duststorm512 = double(imread('Dust512.tiff'));
-cameraman = double(imread('standard_test_images/cameraman.tif'));
-house = imread('standard_test_images/house.tif'); house = double(house(:,:,1));
-jetplane = imread('standard_test_images/jetplane.tif'); jetplane = double(jetplane(:,:,1));
-lake = imread('standard_test_images/lake.tif'); lake = double(lake(:,:,1));
-lena_gray_512 = double(imread('standard_test_images/lena_gray_512.tif'));
-livingroom = double(imread('standard_test_images/livingroom.tif'));
-mandril_gray = double(imread('standard_test_images/mandril_gray.tif'));
 peppers_gray = imread('standard_test_images/peppers_gray.tif'); peppers_gray = double(peppers_gray(:,:,1));
 pirate = double(imread('standard_test_images/pirate.tif'));
-walkbridge = imread('standard_test_images/walkbridge.tif'); walkbridge = double(walkbridge(:,:,1));
-woman_blonde = double(imread('standard_test_images/woman_blonde.tif'));
-woman_darkhair = double(imread('standard_test_images/woman_darkhair.tif'));
 
 % Parameters
-
-image = woman_darkhair;
+image = jetplane;
 wname = 'rbio6.8';
-decomp_level = 3;
+decomp_level = 5;
 dcpf = 20;
 dpz = 100*(1 - 1/dcpf);
 useWavelet = 1; % 0 = wavelet packet
